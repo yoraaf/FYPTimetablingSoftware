71e8b217b8c13ff4cd8b4350a700a35af31b3a74
yoraaf D <44059523+yoraaf@users.noreply.github.com>
2021-04-14 19:55:45 +0100

    Code seems to be able to generate the first generation, then it just breaks completely and uses up 100% of cpu no matter the thread limit.

--- GeneticAlgorithm.cs
+++ GeneticAlgorithm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace FYPTimetablingSoftware {
 	public class GeneticAlgorithm<T> {
@@ -15,19 +16,21 @@ namespace FYPTimetablingSoftware {
 		public float MutationRate;
 
 		private List<DNA<T>> newPopulation;
-		private Random random;
+		public Random random;
 		private float fitnessSum;
-		private int dnaSize;
-		private Func<Klas, T> getRandomGene;
-		private Func<int, float> fitnessFunction;
+		public int dnaSize;
+		public Func<Klas, T> getRandomGene;
+		public Func<int, float> fitnessFunction;
 		private Action updateAlgorithm;
 		private readonly Klas[] KlasArr;
+		private ManualResetEvent[] DoneEvents;
 
 		public GeneticAlgorithm(int populationSize, int dnaSize, Random random, Func<Klas, T> getRandomGene, Func<int, float> fitnessFunction, Action updateAlgorithm,
 			int elitism, float mutationRate = 0.01f) {
 			Generation = 1;
 			Elitism = elitism;
 			MutationRate = mutationRate;
+			NewMember<T>.ga = this;
 			Population = new List<DNA<T>>(populationSize);
 			newPopulation = new List<DNA<T>>(populationSize);
 			this.random = random;
@@ -37,7 +40,7 @@ namespace FYPTimetablingSoftware {
 			this.updateAlgorithm = updateAlgorithm;
 			BestGenes = new T[dnaSize];
 			KlasArr = XMLParser.GetKlasList(); //The parser must be ran before the algorithm starts
-
+			ThreadPool.SetMaxThreads(10, 10);
 			//When the genetic algorithm is created the initial population is generated as follows:
 			for (int i = 0; i < populationSize; i++) {
 				Population.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
@@ -53,8 +56,8 @@ namespace FYPTimetablingSoftware {
             }
 			return output;
         }
-		public void NewGeneration(int numNewDNA = 0, bool crossoverNewDNA = false) {
-			int finalCount = Population.Count + numNewDNA;
+		public void NewGeneration() {
+			int finalCount = Population.Count;
 
 			if (finalCount <= 0) {
 				return;
@@ -64,32 +67,58 @@ namespace FYPTimetablingSoftware {
 				CalculateFitness();
 				Population.Sort(CompareDNA);
 			}
+			NewMember<T>.ShouldWait = true;
+			//int counter = 500;
+            NewMember<T>.counter = 500;
+			var doneEvent = new ManualResetEvent(false);
+			NewMember<T>.DoneEvt = doneEvent;
 			newPopulation.Clear();
-
-			for (int i = 0; i < Population.Count; i++) {
-				if (i < Elitism && i < Population.Count) { //elitism makes it so that the top (5) make it into the next generation 
-					newPopulation.Add(Population[i]);
-				} else if (i < Population.Count || crossoverNewDNA) {
-					DNA<T> parent1 = ChooseParent();
-					DNA<T> parent2 = ChooseParent();
-
-					DNA<T> child = parent1.Crossover(parent2);
-
-					child.Mutate(MutationRate);
-					/*Debug.WriteLine("Parent 1: "+ ArrayToString(parent1.Genes));
-					Debug.WriteLine("Parent 2: "+ ArrayToString(parent2.Genes));
-					Debug.WriteLine("Child:    "+ ArrayToString(child.Genes));*/
-					newPopulation.Add(child);
-				} else {
-					newPopulation.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
-				}
+			DoneEvents = new ManualResetEvent[Population.Count];
+			var newMembersArr = new NewMember<T>[Population.Count];
+			for(int i = 0; i < Population.Count; i++) {
+				newMembersArr[i] = new NewMember<T>(i);
 			}
+			for (int i = 0; i < Population.Count; i++) {
+				ThreadPool.QueueUserWorkItem(newMembersArr[i].GenerateNewMember);
+				Console.WriteLine(">>>added thread "+i);
+			}
+			NewMember<T>.ShouldWait = false;
+			doneEvent.WaitOne();
+			Console.WriteLine("All members done");
 
+			for(int i = 0; i < Population.Count; i++) {
+				newPopulation.Add(newMembersArr[i].Member);
+            }
+			Debug.WriteLine("new population added");
 			List<DNA<T>> tmpList = Population;
 			Population = newPopulation;
 			newPopulation = tmpList;
 
 			Generation++;
+		}
+
+		private void NewMember(Object threadContext) {
+			object[] arr = threadContext as object[];
+			int memberPosition = (int)arr[0];
+			ManualResetEvent doneEvt = (ManualResetEvent)arr[1];
+			if (memberPosition < Elitism && memberPosition < Population.Count) { //elitism makes it so that the top (5) make it into the next generation 
+				newPopulation.Add(Population[memberPosition]);
+			} else if (memberPosition < Population.Count) {
+				DNA<T> parent1 = ChooseParent();
+				DNA<T> parent2 = ChooseParent();
+
+				DNA<T> child = parent1.Crossover(parent2);
+
+				child.Mutate(MutationRate);
+				/*Debug.WriteLine("Parent 1: "+ ArrayToString(parent1.Genes));
+				Debug.WriteLine("Parent 2: "+ ArrayToString(parent2.Genes));
+				Debug.WriteLine("Child:    "+ ArrayToString(child.Genes));*/
+				newPopulation.Insert(memberPosition, child);
+			} else {
+				newPopulation.Insert(memberPosition, new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
+			}
+			Console.WriteLine("[ThreadPool] Thread " + memberPosition + " completed");
+			doneEvt.Set();
 		}
 
 		private int CompareDNA(DNA<T> a, DNA<T> b) {
---
+++ NewMember.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FYPTimetablingSoftware {
+    public class NewMember<T> {
+        public static ManualResetEvent DoneEvt;
+        private int N;
+        public static int counter;
+        public static bool ShouldWait = true;
+        public DNA<T> Member { get; private set; }
+        //public static List<DNA<T>> Population { get; set; } // T is SolutionGene
+        //public static int Elitism;
+        //public static Random random;
+        public static GeneticAlgorithm<T> ga;
+        public NewMember(int n) {
+            N = n;
+            //DoneEvt = doneEvt;
+        }
+
+        public void GenerateNewMember(object threadContext) {
+            while (ShouldWait) {
+                Thread.Sleep(1);
+            }
+            if (N < ga.Elitism && N < ga.Population.Count) { //elitism makes it so that the top (5) make it into the next generation 
+                Member = ga.Population[N];
+            } else if (N < ga.Population.Count) {
+                DNA<T> parent1 = ChooseParent();
+                DNA<T> parent2 = ChooseParent();
+
+                DNA<T> child = parent1.Crossover(parent2);
+
+                child.Mutate(ga.MutationRate);
+                /*Debug.WriteLine("Parent 1: "+ ArrayToString(parent1.Genes));
+				Debug.WriteLine("Parent 2: "+ ArrayToString(parent2.Genes));
+				Debug.WriteLine("Child:    "+ ArrayToString(child.Genes));*/
+                Member = child;
+            } else {
+                Member= new DNA<T>(ga.dnaSize, ga.random, ga.getRandomGene, ga.fitnessFunction, shouldInitGenes: true);
+            }
+            Console.WriteLine("[ThreadPool] Thread " + N + " completed, left: "+counter);
+            if (Interlocked.Decrement(ref counter) == 0) { 
+                DoneEvt.Set();
+            };
+        }
+
+        private static DNA<T> ChooseParent() {
+            //tournament style selection
+            double randomNumber = GetFitnessSum() * ga.random.NextDouble();
+            int tournamentSize = (Int32)Math.Floor(ga.Population.Count * 0.2);
+            DNA<T>[] tournamentMembers = new DNA<T>[tournamentSize];
+            for (int i = 0; i < tournamentSize; i++) {
+                DNA<T> x;
+                do {
+                    //add random person from population to the tournament and make sure they aren't in already
+                    x = ga.Population[ga.random.Next(0, ga.Population.Count)];
+                } while (tournamentMembers.Contains(x));
+                tournamentMembers[i] = x;
+            }
+            //sort tournament members my fitness and return the fittest one
+            Array.Sort(tournamentMembers, CompareDNA);
+            //Console.WriteLine("top ")
+            return tournamentMembers[0];
+        }
+        private static double GetFitnessSum() {
+            double output = 0;
+            for (int i = 0; i < ga.Population.Count; i++) {
+                output += ga.Population[i].Fitness;
+            }
+            return output;
+        }
+        private static int CompareDNA(DNA<T> a, DNA<T> b) {
+            //had to switch the >< around cz low = better
+            if (a.Fitness < b.Fitness) {
+                return -1;
+            } else if (a.Fitness > b.Fitness) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
